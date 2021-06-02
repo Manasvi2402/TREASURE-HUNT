35c45c179a244f3d972e6ca59076b7787aa1eac5
Manasvi2402 <74135076+Manasvi2402@users.noreply.github.com>
2021-06-02 20:24:40 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+  //  code for displaying rest of the names and the hints.  
+    fill("white")
+    textSize(20)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,80)
+
+    fill("white")
+    textSize(20)
+    text("A T A D A S B A",690,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!",690,180)
+ 
+    fill("white")
+    textSize(20)
+    text("C U T N I F O N",100,300)
+    fill("lightblue")
+    text("Hint: Stores all information !!",100,330)
+ 
+}
